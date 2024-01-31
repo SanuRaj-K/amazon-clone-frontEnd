51085e122fab6f5cb4153d79f687ed1e627a2a77
SanuRaj-K <sanusrk29@gmail.com>
2024-01-31 14:08:23 +0530

    fix

--- src/Pages/Admin/Home.jsx
+++ src/Pages/Admin/Home.jsx
@@ -18,7 +18,7 @@ const AdminHomePage = () => {
       .get("https://amazon-clone-backend-fz8l.onrender.com/admin/stats")
       .then((res) => {
         setData(res.data);
-        toast.success("sucessfully fetched", { id: toastId });
+        toast.remove(toastId);
       })
       .catch((err) => {
         console.log(err);
